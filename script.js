4804ff003ec384ac8f1e7200bb5d9de3ee8a6b60
Jeison Moura <85846383+jeimoal@users.noreply.github.com>
2022-08-01 15:52:45 -0300

    Update script.js

--- script.js
+++ script.js
@@ -1,23 +1,56 @@
-const btnClick = document.getElementById('root');
-const btn = document.createElement('button');
-btn.innerHTML = 'Gerar número da sorte';
-btnClick.appendChild(btn);
+let betNum = {
+dozen: null, //create html element
+unit: null, //create html element
+matrix: [], //stores the numbers
 
-btn.addEventListener('click', luckyNum);
+init: () => {
+	//create and set the id to wrapper
+	body = document.querySelector('body');
+	divW = document.createElement('div');
+	divW.setAttribute('id','betWrapper');
+	body.appendChild(divW);
 
-function luckyNum(){
-let ct = 0;
-let luck;
-let ph = "Seus números da sorte são: ";  
-let para = document.getElementById('root');
-let txtPh = document.createTextNode(ph);
-para.appendChild(txtPh);
-  
-for(ct; ct < 6; ct++) {  
-  const array = Math.floor(Math.random() * 10);
-  luck = array;    
-  gra = document.createElement('p');
-  txt = document.createTextNode(luck);
-  para.appendChild(txt);
+	//create lucky container
+	luckyBetCont = document.createElement('div');
+	luckyBetCont.setAttribute('id','luckyBetCont');
+	divW.appendChild(luckyBetCont);
+
+	//create lucky button
+	luckyBtn = document.createElement('button');
+	luckyBtn.setAttribute('id', 'luckyBtn');
+	luckyBtn.textContent = "Gerar números da sorte";
+	luckyBetCont.appendChild(luckyBtn);
+	luckyBtn.addEventListener('click', betNum.luckyBet);
+
+	//create lucky num div
+	luckyNum = document.createElement('div');
+	luckyTxt = document.createElement('p')
+	luckyNum.setAttribute('id', 'luckyNum');
+	luckyTxt.setAttribute('id', 'luckyBet');
+	luckyNum.appendChild(luckyTxt);
+	luckyBetCont.appendChild(luckyNum);
+},
+
+//generate lucky bet numbers
+luckyBet: () => {
+let matrix = document.querySelector('#luckyBet');
+let matrixTxt;
+let matrixRep;
+while (betNum.matrix.length != 6) {
+	betNum.dozen = Math.floor(Math.random() * 10);
+	betNum.unit = Math.floor(Math.random() * 10);
+	concat = `${betNum.dozen}${betNum.unit} `;
+
+	if(concat > 0 && concat <=60) {
+		betNum.matrix.unshift(concat);
+	}
+	matrixTxt = betNum.matrix;
+	//matrixRep = matrixTxt.replace(",", " "); does not work. Searching for a solution...
+	matrix.textContent = matrixTxt;
 }
-};
+
+},
+
+}
+
+window.addEventListener('load', betNum.init);
